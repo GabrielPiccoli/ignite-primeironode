a47248e5d70c6461b1e0319bfeda7a5bfd783461
TBRWEB - Desenvolvimento de Sites <tbrweb01@gmail.com>
2021-12-06 17:48:43 -0300

    Get Statement Route

--- src/index.js
+++ src/index.js
@@ -8,16 +8,31 @@ app.use(express.json())
 
 app.post("/account", (req, res) => {
   const { cpf, nome } = req.body
-  const id = uuidv4()
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
 
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists" })
+  }
+  
   customers.push({
     cpf, 
     nome,
-    id,
+    id: uuidv4(),
     statement: []
   })
 
   return res.status(201).send()
+})
+
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" })
+  }
+
+  return res.json(customer.statement)
 })
 
 app.listen(3333, () => console.log("Server is Running"))

25492f9ddf3811466c123420ed48718b6e2561e9
TBRWEB - Desenvolvimento de Sites <tbrweb01@gmail.com>
2021-12-07 17:52:08 -0300

    New Routes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express")
 const express = require("express")
 const { v4: uuidv4 } = require("uuid")
 
@@ -5,6 +6,30 @@ const customers = []
 const app = express()
 
 app.use(express.json())
+
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" })
+  }
+
+  req.customer = customer
+  return next()
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => { 
+    if (operation.type === 'credit') {
+      return acc + operation.amount
+    } else {
+      return acc - operation.amount
+    }
+  }, 0)
+
+  return balance
+}
 
 app.post("/account", (req, res) => {
   const { cpf, nome } = req.body
@@ -24,15 +49,54 @@ app.post("/account", (req, res) => {
   return res.status(201).send()
 })
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return res.status(400).json({ error: "Customer not found" })
-  }
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req
 
   return res.json(customer.statement)
+})
+
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body
+  const { customer } = req
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation)
+  
+  return res.status(201).send()
+})
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+  const { amount } = req.body
+  const { customer } = req
+  const balance = getBalance(customer.statement)
+
+  if (balance < amount) {
+    return res.status(400).json({ error: "Insufficient funds!" })
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return res.status(201).send()
+})
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req
+  const { date } = req.query
+  const dateFormat = new Date(date + " 00:00")
+  const statement = customer.statement.filter(statement => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return res.json(statement)
 })
 
 app.listen(3333, () => console.log("Server is Running"))
